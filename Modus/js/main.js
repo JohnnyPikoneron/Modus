e4d671acf3f66873ff74fa42d7bd9ce1eccab183
JohnnyPikoneron <dj_serje@inbox.ru>
2017-01-18 17:21:28 +0200

    new

---
+++ Modus/js/main.js
@@ -0,0 +1,52 @@
+﻿$(document).ready(function(){
+
+    $('.slider').slick({
+        dots: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+	
+	$('.slider2').slick({
+        speed: 500,
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+			{
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
+    $("#menu").mmenu({
+        "slidingSubmenus": false,
+        "extensions": [
+            "pagedim-black",
+            "theme-dark"
+        ],
+        "offCanvas": {
+            "position": "right"
+        }
+    });
+});
+
+
+
